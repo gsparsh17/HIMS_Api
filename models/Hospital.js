b8353710c04b4ec30124f0805aafc6807ebc0790
Sparsh <gsparsh17@gmail.com>
2026-01-21 19:06:42 +0530

    pincode fixed

--- models/Hospital.js
+++ models/Hospital.js
@@ -5,12 +5,15 @@ const hospitalSchema = new mongoose.Schema({
   hospitalID: { type: String, required: true, unique: true }, // e.g., AB1234
   registryNo: { type: String, required: true },
   hospitalName: { type: String, required: true }, // Changed from name
-  logo: { type: String }, // URL from Cloudinary
+  logo: { type: String }, 
   companyName: { type: String }, // Optional
   companyNumber: { type: String }, // Optional
   name: { type: String, required: true }, // Contact person name
   address: { type: String, required: true },
   contact: { type: String, required: true },
+  pinCode: { type: String },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
   email: { type: String, required: true },
   fireNOC: { type: String }, // Optional
   policyDetails: { type: String }, // Optional
