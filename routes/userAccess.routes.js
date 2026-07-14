31585c5ce76941e62a003ec3fd3a6be7e4bb8e08
Pranshu2404 <pranshu7905@gmail.com>
2026-07-15 02:48:25 +0530

    Fix Express middleware pollution blocking Super Admin login

--- routes/userAccess.routes.js
+++ routes/userAccess.routes.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const { protect, authorize } = require('../middlewares/auth');
 const controller = require('../controllers/userAccess.controller');
 
-// All routes require authentication and user access management permission
-router.use(protect);
-router.use(authorize('admin', 'mediqliq_super_admin'));
+const requireAuth = [protect, authorize('admin', 'mediqliq_super_admin')];
 
 // Get all users
-router.get('/users', controller.getUsers);
+router.get('/users', requireAuth, controller.getUsers);
 
 // Create new user
-router.post('/users', controller.createUser);
+router.post('/users', requireAuth, controller.createUser);
 
 // Update user permissions
-router.put('/users/:userId/permissions', controller.updateUserPermissions);
+router.put('/users/:userId/permissions', requireAuth, controller.updateUserPermissions);
 
 // Reset user password
-router.put('/users/:userId/password', controller.resetPassword);
+router.put('/users/:userId/password', requireAuth, controller.resetPassword);
 
 module.exports = router;
