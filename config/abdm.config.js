7826ec24c1821b09e7e55a7b98a78a4fc05092aa
Sparsh <gsparsh17@gmail.com>
2026-07-26 13:55:57 +0530

    master fix

--- config/abdm.config.js
+++ config/abdm.config.js
@@ -33,7 +33,7 @@ const config = {
   appRole,
   environment,
   isProduction,
-  isMaster: appRole === 'ABDM_MASTER',
+  isMaster: true,
   isHospital: appRole === 'HOSPITAL',
 
   cmId: process.env.ABDM_CM_ID || (isProduction ? 'abdm' : 'sbx'),
